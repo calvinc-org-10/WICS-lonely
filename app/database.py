55c434e472cbadce0b4f6458208759293a668141
Calvin C <curtindolph.calvin.10@gmail.com>
2025-10-29 22:15:23 -0500

    create app folder and app/database.py

---
+++ app/database.py
@@ -0,0 +1,24 @@
+# from PySide6.QtSql import (QSqlDatabase, QSqlQuery )
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+rootdir = "."
+app_dbName = f"{rootdir}\\WICSdb.sqlite"
+
+# an Engine, which the Session will use for connection
+# resources, typically in module scope
+app_engine = create_engine(
+    f"sqlite:///{app_dbName}",
+    )
+# a sessionmaker(), also in the same scope as the engine
+app_Session = sessionmaker(app_engine)
+
+def get_app_session():
+    return app_Session()
+
+##########################################################
+###################    REPOSITORIES    ###################
+##########################################################
+
+from cMenu.database import Repository
