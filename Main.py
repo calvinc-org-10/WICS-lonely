cee202045fb45e252097e90387c53317cd4e6a25
Calvin C <curtindolph.calvin.10@gmail.com>
2026-05-24 07:33:30 -0500

    bringing WICS-lonely up to speed I've neglected this app for too long...

--- Main.py
+++ Main.py
@@ -1,11 +1,58 @@
 import sys
-from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import (QCoreApplication, )
+from PySide6.QtWidgets import (
+    QApplication, 
+    QWidget, QVBoxLayout, 
+    )
+from PySide6.QtGui import QPixmap
+
+from calvincTools import calvincTools
+
+from sysver import (_appname, sysver, )
+from app_secrets import (sysver_key, usr_auth_required, )
+
+from menuformname_viewMap import FormNameToURL_Map
+from externalWebPageURL_Map import ExternalWebPageURL_Map
+from app.database import get_app_sessionmaker
+
+class MainScreen(QWidget):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        if not self.objectName():
+            self.setObjectName("MainWindow")
+
+        pixsize=100
+        # applogo = QPixmap("F:/calvincTools/calvincTools/assets/cTools.png").scaled(pixsize, pixsize, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation),
+        applogo = QPixmap()
+        cTools = calvincTools(
+            app_sessionmaker=get_app_sessionmaker(),
+            FormNameToURL_Map=FormNameToURL_Map,
+            ExternalWebPageURL_Map=ExternalWebPageURL_Map,
+            appname=_appname,
+            logo=applogo,
+            appver=sysver[sysver_key],
+            usr_auth=usr_auth_required,
+            )
+
+        llayout = QVBoxLayout(self)
+        stack = cTools.main_window_stack()
+        if stack is not None:
+            llayout.addWidget(stack)
+        self.setLayout(llayout)
+
+        self.setWindowTitle(QCoreApplication.translate("MainWindow", _appname, None))
+
+        cTools.login()
+        
+    # __init__
+# MainScreen
+    def end_of_class(self):
+        pass
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    from MainScreen import MainScreen   # QApplication must exist before this import (qt.sql.qsqldatabase: QSqlDatabase requires a QCoreApplication)
     topscreen = MainScreen()
     topscreen.show()
 
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,8 +2,8 @@
 from PySide6.QtWidgets import QLabel
 std_id_def = {'label': 'ID', 'widgetType': QLabel, 'noedit': True, 'readonly': True, 'position': (0,0)}
 
-from .fmActualCounts import *
-from .frmCountSchedule import *
-from .frmMaterials import *
-from .frmPartTypes import *
-from .spreadsheet import *
+# from .fmActualCounts import *
+# from .frmCountSchedule import *
+# from .frmMaterials import *
+# from .frmPartTypes import *
+# from .spreadsheet import *
