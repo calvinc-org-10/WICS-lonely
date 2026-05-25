c822a13dc0b4b71d66360e0e3a56d8f6fcfb322b
Calvin C <curtindolph.calvin.10@gmail.com>
2026-05-25 11:31:34 -0500

    WIP - fixing calls to calvincTools

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,8 +2,8 @@
 from PySide6.QtWidgets import QLabel
 std_id_def = {'label': 'ID', 'widget_type': QLabel, 'readonly': True, 'position': (0,0)}
 
-# from .fmActualCounts import *
-# from .frmCountSchedule import *
-# from .frmMaterials import *
-# from .frmPartTypes import *
-# from .spreadsheet import *
+from .fmActualCounts import *
+from .frmCountSchedule import *
+from .frmMaterials import *
+from .frmPartTypes import *
+from .spreadsheet import *
